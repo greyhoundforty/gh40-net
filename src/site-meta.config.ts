0380eb22cad2d0967f9f2d18ed0b9671937bd400
Ryan Tiffany <ryantiffany@fastmail.com>
2022-10-20 09:19:13 -0500

    pushing with astro cactus theme

---
+++ src/site-meta.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+  title: "gh40.net",
+  description: "Mostly harmless since 1977.",
+  lang: "en-US",
+  ogLocale: "en_US",
+  githubUrl: "https://github.com/greyhoundforty/gh40-net",
+  themeColorLight: "#fafafa",
+  themeColorDark: "#1d1f21",
+}
+
+export default siteMeta;
